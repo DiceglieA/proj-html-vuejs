4825051c85257133610a8675ef8236eb94fb7da3
DiceglieA <angelodiceglie10@gmail.com>
2022-12-09 17:55:16 +0100

    last commit

--- src/components/CardstaffComp.vue
+++ src/components/CardstaffComp.vue
@@ -2,8 +2,9 @@
   <div>
     <div class="staff-card row mb-5">
       <div class="left col-6">
-        <div>
+        <div class="img-plus">
           <img class="w-100" :src="require(`../assets/img/images/${staff.image}`)" alt="">
+          <div class="hover-plus text-center">+</div>
         </div>
         <div class="text-center py-2 hover-violet">
           <div class="violet">{{staff.name}}</div>
@@ -54,5 +55,24 @@ props: {
 }
 .left:hover .hover-violet{
   background-color: #e8e6e7;
+}
+.img-plus {
+  position: relative;
+}
+.hover-plus {
+  background-color: rgb(76, 74, 155, 0.2);
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  color: white;
+  position: absolute;
+  top: 40%;
+  left: 40%;
+  z-index: 10;
+  font-size: 35px;
+  display: none;
+}
+.left:hover .hover-plus {
+  display: block;
 }
 </style>
