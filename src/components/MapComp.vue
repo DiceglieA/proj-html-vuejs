c693304398e6a25da70c8bab54b4041605de4475
DiceglieA <angelodiceglie10@gmail.com>
2022-12-09 17:11:32 +0100

    implementata mappa

--- src/components/MapComp.vue
+++ src/components/MapComp.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="row">
-    <div class="">
-
+  <div class="w-100">
+    <div class="mapouter w-100">
+      <div class="gmap_canvas">
+      <iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+      <a href="https://123movies-to.org"></a>
+      <br>
+      <a href="https://www.embedgooglemap.net">embed google map in html</a>
     </div>
+  </div>
   </div>
 </template>
 
@@ -12,6 +17,15 @@ name: 'MapComp'
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.mapouter{
+  position:relative;
+  text-align:right;
+  height:500px;width:100%;
+}
+.gmap_canvas {
+  overflow: hidden;
+  background:none!important; 
+  height:500px;width:100%;
+  }
 </style>
